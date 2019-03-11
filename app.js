04ec507f1a69e9c04122fd248d3a38a9da48c2b4
saulwolfcode <43302778+saulwolfcode@users.noreply.github.com>
2019-03-11 00:32:53 -0300

    Creando entorno completo, separando la informacion

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const path = require("path")
+const bodyParser = require("body-parser")
 const app = express()
-
+const routes = require("./routes/index")
 
 //SETTINGS
 app.set("port", process.env.PORT || 3000)
@@ -11,21 +12,27 @@ app.set("port", process.env.PORT || 3000)
 //path.join= une direcctorios
 
 app.set("views", path.join(__dirname, "views"))
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 
 
 
 //MIDDLEWARES
+app.use((req, res, next) => {
+    console.log(`${req.url} -- ${req.method}`)
+    next()
+})
 
-
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 //ROUTES
 
-
+app.use(routes)
 
 //FILES STATICS
 
-
+app.use(express.static(path.join(__dirname, "public")))
 
 
 //START SERVER
